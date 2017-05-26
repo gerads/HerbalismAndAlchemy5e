39a0b5e93cfca1ed8ed178901b8a66114be32ab2
Paul Gerads <geradsp0833@my.uwstout.edu>
2017-05-26 01:27:49 -0500

    Changing namespace so tests don't get included in coverage.

--- Herbalism.Tests.Unit/HerbalismTests.cs
+++ Herbalism.Tests.Unit/HerbalismTests.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace HerbalismAndAlchemy.Tests
+namespace HerbalismAndAlchemyTests
 {
     [TestFixture()]
     public class HerbalismTests
